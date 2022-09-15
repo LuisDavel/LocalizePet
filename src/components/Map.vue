c3bdf218f17f76de7ea3cea802a2311fcb553d6e
Luis Davel <42303034+LuisDavel@users.noreply.github.com>
2022-09-15 13:31:53 -0300

    Update Map.vue
    v2

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,5 @@
 <template>
-   <div id = "map">
+    <div id="map">
     </div>
 </template>
 <script>
@@ -14,51 +14,36 @@ import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
 
 export default {
     name: 'Map',
-    data(){
-        return {
-            lat: 0,
-            long: 0
-        }
-    },
-    methods:{
-        onLoad:function(){
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(this.showPosition);
-                console.log("--- Rodou o get Location ---");
-            } else {
-                console.log("---  Não rodou ---");
-            }
+    methods: {
+        async onLoad() {
+            return new Promise((resolve, reject) => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(pos => {
+                        resolve(pos)
+                    })
+                }
+            })
         },
-        showPosition: function(position) {
-            this.lat = position.coords.latitude;
-                console.log(this.lat)
-                //localStorage.setItem("lat", this.lat);
-            this.long = position.coords.longitude;
-                console.log(this.long)
-            }
     },
-    mounted() {
-        this.onLoad()
-
-        console.log(parseInt(this.lat) + " vv")
-        //this.showPosition()
-
+    async mounted() {
+        let data = await this.onLoad()
+        
         const circleFeature = new Feature({
-            geometry: new Circle([ this.long,  this.lat], 0.001)
+            geometry: new Circle([data.coords.longitude, data.coords.latitude], 0.001)
         });
 
         const pointFeature = new Feature({
-            geometry: new Point([-49.374100250480424, -28.679954764955344])
+            geometry: new Point([data.coords.longitude, data.coords.latitude])
         });
 
         pointFeature.setStyle(
             new Style({
                 image: new Icon({
-                anchor: [0.5, 1],
-                anchorXUnits: "fraction",
-                //anchorXUnits: "pixels",
-                scale: 0.05,
-                src: "https://cdn-icons-png.flaticon.com/512/684/684908.png"
+                    anchor: [0.5, 1],
+                    anchorXUnits: "fraction",
+                    //anchorXUnits: "pixels",
+                    scale: 0.05,
+                    src: "https://cdn-icons-png.flaticon.com/512/684/684908.png"
                 })
             })
         );
@@ -66,28 +51,28 @@ export default {
         circleFeature.setStyle(
             new Style({
                 renderer(coordinates, state) {
-                const [[x, y], [x1, y1]] = coordinates;
-                const ctx = state.context;
-                const dx = x1 - x;
-                const dy = y1 - y;
-                const radius = Math.sqrt(dx * dx + dy * dy);
-                const innerRadius = 0;
-                const outerRadius = radius * 1.1;
-                const gradient = ctx.createRadialGradient(
-                    x,
-                    y,
-                    innerRadius,
-                    x,
-                    y,
-                    outerRadius
-                );
-                gradient.addColorStop(0, "rgba(30,144,255,0)");
-                gradient.addColorStop(0.5, "rgba(30,144,255,0.1)");
-                gradient.addColorStop(1, "rgba(30,144,255,0.3)");
-                ctx.beginPath();
-                ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
-                ctx.fillStyle = gradient;
-                ctx.fill();
+                    const [[x, y], [x1, y1]] = coordinates;
+                    const ctx = state.context;
+                    const dx = x1 - x;
+                    const dy = y1 - y;
+                    const radius = Math.sqrt(dx * dx + dy * dy);
+                    const innerRadius = 0;
+                    const outerRadius = radius * 1.1;
+                    const gradient = ctx.createRadialGradient(
+                        x,
+                        y,
+                        innerRadius,
+                        x,
+                        y,
+                        outerRadius
+                    );
+                    gradient.addColorStop(0, "rgba(30,144,255,0)");
+                    gradient.addColorStop(0.5, "rgba(30,144,255,0.1)");
+                    gradient.addColorStop(1, "rgba(30,144,255,0.3)");
+                    ctx.beginPath();
+                    ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+                    ctx.fillStyle = gradient;
+                    ctx.fill();
                 }
             })
         );
@@ -95,18 +80,18 @@ export default {
         this.map = new Map({
             layers: [
                 new TileLayer({
-                source: new OSM(),
-                visible: true
+                    source: new OSM(),
+                    visible: true
                 }),
                 new VectorLayer({
-                source: new VectorSource({
-                    features: [pointFeature, circleFeature]
-                })
+                    source: new VectorSource({
+                        features: [pointFeature, circleFeature]
+                    })
                 })
             ],
             target: "map",
             view: new View({
-                center: [-49.374100250480424, -28.679954764955344],
+                center: [data.coords.longitude, data.coords.latitude],
                 zoom: 17,
                 projection: "EPSG:4326"
             })
@@ -114,4 +99,4 @@ export default {
     },
 
 }
-  </script>
+</script>
